3a91f24c2b6418a5e74896f8bda35608719b5c12
DESKTOP-R1SMO72\campx <toisekhongcamchiu@gmail.com>
2025-12-30 08:20:21 +0700

    add file

---
+++ CoinAnalysis/FrmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinAnalysis
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmMain_Shown(object sender, EventArgs e)
+        {
+            Process pc_chrome = new Process();
+            pc_chrome.StartInfo.FileName = ".\\runtimes\\win\\native\\selenium-manager.exe";
+            pc_chrome.StartInfo.Arguments = "--browser chrome";
+            pc_chrome.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pc_chrome.Start();
+            pc_chrome.WaitForExit();
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+    }
+}
