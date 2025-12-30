191c5e00f6c2c5a31227e8a956f3953998712550
DESKTOP-R1SMO72\campx <toisekhongcamchiu@gmail.com>
2025-12-30 11:09:03 +0700

    update

--- CoinAnalysis/FrmMain.Designer.cs
+++ CoinAnalysis/FrmMain.Designer.cs
@@ -28,13 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOpenProfile = new System.Windows.Forms.Button();
+            this.buttonOpenDriver = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(33, 16);
+            this.buttonOpenFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(115, 36);
+            this.buttonOpenFolder.TabIndex = 0;
+            this.buttonOpenFolder.Text = "Open folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonOpenProfile
+            // 
+            this.buttonOpenProfile.Location = new System.Drawing.Point(156, 16);
+            this.buttonOpenProfile.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonOpenProfile.Name = "buttonOpenProfile";
+            this.buttonOpenProfile.Size = new System.Drawing.Size(115, 36);
+            this.buttonOpenProfile.TabIndex = 2;
+            this.buttonOpenProfile.Text = "Open profile";
+            this.buttonOpenProfile.UseVisualStyleBackColor = true;
+            this.buttonOpenProfile.Click += new System.EventHandler(this.buttonOpenProfile_Click);
+            // 
+            // buttonOpenDriver
+            // 
+            this.buttonOpenDriver.Location = new System.Drawing.Point(279, 16);
+            this.buttonOpenDriver.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonOpenDriver.Name = "buttonOpenDriver";
+            this.buttonOpenDriver.Size = new System.Drawing.Size(115, 36);
+            this.buttonOpenDriver.TabIndex = 3;
+            this.buttonOpenDriver.Text = "Open driver";
+            this.buttonOpenDriver.UseVisualStyleBackColor = true;
+            this.buttonOpenDriver.Click += new System.EventHandler(this.buttonOpenDriver_Click);
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(604, 341);
+            this.Controls.Add(this.buttonOpenDriver);
+            this.Controls.Add(this.buttonOpenProfile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonOpenFolder);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmMain";
             this.Text = "Coin Analysis";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
@@ -45,6 +98,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpenFolder;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOpenProfile;
+        private System.Windows.Forms.Button buttonOpenDriver;
     }
 }
 
--- CoinAnalysis/FrmMain.cs
+++ CoinAnalysis/FrmMain.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,6 +20,14 @@ namespace CoinAnalysis
 {
     public partial class FrmMain : Form
     {
+        public static string FolderUserData = Path.Combine(Application.StartupPath, "ChromeUserData");
+
+        public static ChromeDriver driver = null;
+        public static ReadOnlyCollection<IWebElement> elements = null;
+        public static IWebElement element = null;
+        public static Actions actions = null;
+        public static IJavaScriptExecutor js;
+
         public FrmMain()
         {
             InitializeComponent();
@@ -38,5 +53,176 @@ namespace CoinAnalysis
         {
 
         }
+
+        private void StartChromeDriver()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            ChromeOptions options = new ChromeOptions();
+
+            options.AddArgument($"--user-data-dir={FolderUserData}");
+
+            options.AddArgument("--start-maximized");
+            //options.AddArgument("--disable-notifications");
+            //options.AddArgument("--disable-infobars");
+            //options.AddArgument("--disable-popup-blocking");
+
+            options.AddUserProfilePreference("credentials_enable_service", false);
+            options.AddUserProfilePreference("profile.password_manager_enabled", false);
+
+            // Undetected webdriver
+            options.AddArgument("--disable-blink-features=AutomationControlled");
+            options.AddExcludedArgument("enable-automation");
+            options.AddExcludedArguments(new List<string>() { "enable-automation" });
+            options.AddAdditionalChromeOption("useAutomationExtension", false);
+
+            driver = new ChromeDriver(service, options);
+
+            js = (IJavaScriptExecutor)driver;
+            actions = new Actions(driver);
+        }
+
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", Application.StartupPath);
+        }
+
+        private void buttonOpenProfile_Click(object sender, EventArgs e)
+        {
+            Process pc_chrome = new Process();
+            pc_chrome.StartInfo.FileName = "chrome.exe";
+            pc_chrome.StartInfo.Arguments = $"https://www.binance.com/vi --start-maximized --user-data-dir={FolderUserData}";
+            pc_chrome.Start();
+        }
+
+        private void buttonOpenDriver_Click(object sender, EventArgs e)
+        {
+            StartChromeDriver();
+            for (int i = 0; i < 60 * 60; i++)
+            {
+                Thread.Sleep(1000);
+                try
+                {
+                    driver.FindElement(By.CssSelector("body, div"));
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+            driver.Quit();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> listCoinCode = new List<string>();
+
+            string[] listCoinExclude = File.ReadAllLines("setting_coin_exclude.txt");
+
+            // start chrome driver
+            StartChromeDriver();
+            Thread.Sleep(1000);
+
+            /*
+            for (int page = 1; page <= 5; page++)
+            {
+                driver.Navigate().GoToUrl($"https://coinmarketcap.com/?page={page}");
+                Thread.Sleep(2000);
+
+                element = driver.FindElement(By.CssSelector("[data-role='select-trigger']"));
+
+                // hover
+                js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'})", element);
+                Thread.Sleep(500);
+                action.MoveToElement(element).MoveByOffset(1, 1).Perform();
+                Thread.Sleep(500);
+
+                // scan list
+                elements = driver.FindElements(By.CssSelector("table tbody tr td span.coin-item div[data-nosnippet='true']"));
+                foreach (var element in elements)
+                {
+                    string coinCode = element.Text.Trim();
+                    if (string.IsNullOrEmpty(coinCode))
+                    {
+                        continue;
+                    }
+                    listCoinCode.Add(coinCode);
+                }
+            }
+
+            File.WriteAllLines("list_500_coin.txt", listCoinCode, Encoding.UTF8);
+            */
+            listCoinCode = File.ReadAllLines("list_500_coin.txt", Encoding.UTF8).ToList();
+
+            string folderImage = Path.Combine(Application.StartupPath, "CoinGlass");
+
+            if (!Directory.Exists(folderImage)) Directory.CreateDirectory(folderImage);
+
+            int start = 60;
+
+            for (int i = start; i < listCoinCode.Count; i++)
+            {
+                string coinCode = listCoinCode[i];
+
+                if (listCoinExclude.Contains(coinCode)) continue;
+
+                this.Text = $"{i} - {coinCode}";
+
+                // move mouse
+                Cursor.Position = new Point(0, 0);
+
+                try
+                {
+                    driver.Navigate().GoToUrl($"https://www.coinglass.com/en/pro/futures/LiquidationHeatMap?coin={coinCode}&type=pair");
+                    Thread.Sleep(500);
+
+                    // wait chart
+                    for (var tw = 0; tw < 3; tw++)
+                    {
+                        try
+                        {
+                            element = driver.FindElement(By.CssSelector("canvas[data-zr-dom-id]"));
+                            break;
+                        }
+                        catch (Exception)
+                        {
+                            Thread.Sleep(1000);
+                        }
+                    }
+
+                    element = driver.FindElement(By.CssSelector("canvas[data-zr-dom-id]"));
+
+                    // hover
+                    js.ExecuteScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'})", element);
+                    Thread.Sleep(500);
+
+                    // Chụp màn hình
+                    Screenshot screenshot = ((ITakesScreenshot)driver).GetScreenshot();
+                    Bitmap fullImage = new Bitmap(new System.IO.MemoryStream(screenshot.AsByteArray));
+
+                    // Lấy vị trí & kích thước element
+                    Point location = element.Location;
+                    Size size = element.Size;
+                    // Crop ảnh
+                    Rectangle cropArea = new Rectangle(location.X, 0, size.Width, size.Height);
+                    Bitmap elementImage = fullImage.Clone(cropArea, fullImage.PixelFormat);
+
+                    // lưu ảnh
+                    elementImage.Save(Path.Combine(folderImage, $"cl_{coinCode}.jpg"), System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch (Exception)
+                {
+                    File.AppendAllText("list_coin_error.txt", $"{coinCode}{Environment.NewLine}");
+                    continue;
+                }
+            }
+
+            driver.Quit();
+
+            SystemSounds.Asterisk.Play();
+
+            Application.Exit();
+        }
+
     }
 }
